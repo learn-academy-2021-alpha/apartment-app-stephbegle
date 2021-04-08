78bf138afc3250e60777f5aca2b3ff346a5fdd36
stephbegle <stephbegle@gmail.com>
2021-04-08 16:02:55 -0700

    added controller methods to the apartment controller

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,2 +1,16 @@
 class ApartmentsController < ApplicationController
+
+    def index
+        apartments = Apartment.all
+    end
+
+    def create
+        apartment = Apartment.create(apt_params)
+        (apartment.valid?) ? (render json: apartment) : (render json: apartment.errors, status: :unprocessable_entity)
+    end
+
+    private
+    def apt_params
+        params.require(:apartment).permit(:street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :user_id, :title, :img)
+    end
 end
